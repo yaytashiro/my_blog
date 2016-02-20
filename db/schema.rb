53b015e76a22a269e0967a62cc774946c09e2226
田代やよい <o.841.312@gmail.com>
2016-02-20 11:21:09 +0900

    author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214151218) do
+ActiveRecord::Schema.define(version: 20160220021903) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
